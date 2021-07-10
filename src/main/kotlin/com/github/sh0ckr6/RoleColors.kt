4812fee3c8d00f2be1212acc5554ff682a1ce853
sh0ckR6 <sh0ck.04@outlook.com>
2021-07-10 14:46:01 -0400

    Convert project to pure Kotlin

---
+++ src/main/kotlin/com/github/sh0ckr6/RoleColors.kt
@@ -0,0 +1,39 @@
+package com.github.sh0ckr6
+
+import net.dv8tion.jda.api.JDA
+import javax.security.auth.login.LoginException
+import java.lang.InterruptedException
+import kotlin.jvm.JvmStatic
+import net.dv8tion.jda.api.JDABuilder
+import com.github.sh0ckr6.RoleColors
+
+/**
+ * Main class for the program
+ *
+ * @since 1.0
+ * @author sh0ckR6
+ */
+object RoleColors {
+    /**
+     * The [JDA] representing the bot
+     */
+    private lateinit var bot: JDA
+
+    /**
+     * Main entry point for the program
+     *
+     * @param args The arguments provided by the executor
+     * @throws LoginException If the provided token is invalid
+     * @throws InterruptedException If this thread is interrupted while waiting
+     *
+     * @author sh0ckR6
+     * @since 1.0
+     */
+    @Throws(LoginException::class, InterruptedException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val builder = JDABuilder.createDefault(System.getenv("ROLECOLORSKEY"))
+        bot = builder.build()
+        bot.awaitReady()
+    }
+}
---
+++ src/main/kotlin/com/github/sh0ckr6/Settings.kt
@@ -0,0 +1,10 @@
+package com.github.sh0ckr6
+
+object Settings {
+    /**
+     * The prefix for the bot
+     *
+     * @since 1.0
+     */
+    var prefix = "rc!"
+}
